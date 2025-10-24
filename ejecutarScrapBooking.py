42976577360f7c2f4900b49786948d4b5505d636
Jorge <jorge.herzog17@gmail.com>
2025-10-24 16:50:19 +0200

    Scraping dinamico

---
+++ ejecutarScrapBooking.py
@@ -0,0 +1,12 @@
+from scrapBooking import *
+
+"""
+Script que ejecuta el scraping completo a Booking
+"""
+
+#Parametros ajustables
+url = "https://www.booking.com"
+lugar = "Ibiza"
+
+#Ejecutar código
+ejecutar_script(url, lugar)
